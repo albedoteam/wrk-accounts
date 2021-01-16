d5e2e3f9ab5a2235539bb8965c9537e1b12c0c92
fiduta <eryckson@gmail.com>
2021-01-16 19:39:36 -0300

    init

--- src/_Broker/Contracts/Responses/AccountResponse.cs
+++ src/_Broker/Contracts/Responses/AccountResponse.cs
@@ -6,8 +6,12 @@ namespace Accounts.Contracts.Responses
     {
         string Id { get; set; }
         string Name { get; set; }
-
+        string Description { get; set; }
+        string IdentificationNumber { get; set; }
+        bool Enabled { get; set; }
         DateTime CreatedAt { get; set; }
-        DateTime UpdatedAt { get; set; }
+        DateTime? UpdatedAt { get; set; }
+        bool IsDeleted { get; set; }
+        DateTime? DeletedAt { get; set; }
     }
 }
